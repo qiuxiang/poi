e54e0806ae09f1fd6b30ab2fce890dfcbe1a0720
Qiu Xiang <i@7c00.cc>
2017-04-14 05:50:04 +0800

    使用 async/await 请求模式

--- main.js
+++ main.js
@@ -3,16 +3,31 @@ const mafengwo = require('./mafengwo')
 const NeDB = require('nedb')
 const db = new NeDB({filename: 'database', autoload: true})
 
-// for (let page = 1; page <= 12; page += 1) {
-//   mafengwo.getAttractions(page).then(attractions => {
-//     console.log(page)
-//     attractions.forEach(url => {
-//       db.findOne({url: url}, (e, data) => {
-//         if (data === null) {
-//           mafengwo.getAttractionData(url).then(data => db.insert(data))
-//         }
-//       })
-//     })
-//   })
-// }
-db.find({}, (error, data) => console.log(JSON.stringify(data)))
+async function loadData(url) {
+  return new Promise(resolve =>
+    db.findOne({url: url}, (error, data) => resolve(data))
+  )
+}
+
+async function fetchData(url) {
+  const data = await loadData(url)
+  if (data === null) {
+    const data = await mafengwo.getAttractionData(url)
+    console.log(data)
+    db.insert(data)
+  }
+}
+
+async function main() {
+  for (let page = 1; page <= 63; page += 1) {
+    const attractions = await mafengwo.getAttractions(page)
+    console.log(page)
+    for (let i = 0; i < attractions.length; i += 1) {
+      await fetchData(attractions[i])
+    }
+  }
+}
+
+main()
+
+// db.find({}, (error, data) => console.log(JSON.stringify(data)))
