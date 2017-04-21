8b5a5419b081c80ff30ef0e0573cabab2302f7d4
Qiu Xiang <i@7c00.cc>
2017-04-21 22:27:47 +0800

    优化测试代码

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 const mafengwo = require('./mafengwo')
 const proxies = require('./proxies')
-const request = require('request')
+const request = require('request-promise')
 
 describe('mafengwo', function () {
   this.timeout(6000)
@@ -42,24 +42,16 @@ describe('proxies', function () {
 
   it('check remote ip', async () => {
     for (const i in proxies) {
-      await new Promise((resolve, reject) => {
-        request({
-          url: 'http://ip.cn',
-          proxy: proxies[i],
-          timeout: 6000,
-          headers: {
-            'user-agent': 'curl/7.47.0',
-          },
-        }, (error, response, body) => {
-          console.log(proxies[i])
-          if (!error && body) {
-            console.log(body)
-            resolve()
-          } else {
-            reject(error)
-          }
-        })
+      const body = await request({
+        url: 'http://ip.cn',
+        proxy: proxies[i],
+        timeout: 6000,
+        headers: {
+          'user-agent': 'curl/7.47.0',
+        },
       })
+      console.log(proxies[i])
+      console.log(body)
     }
   })
 })
